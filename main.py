1b62654865df7684994bb57f53acb81263db2533
YukiGros <73797342+AYGros@users.noreply.github.com>
2021-09-09 21:30:32 +0000

    fix "use a Debbugger" using Python Tutor

--- main.py
+++ main.py
@@ -36,12 +36,12 @@
 # total_words = pages * word_per_page
 # print(f"total_words: {total_words}")
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+#Use a Debugger
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
 
-# mutate([1,2,3,5,8,13])
+mutate([1,2,3,5,8,13])
